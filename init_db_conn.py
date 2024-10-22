849549ed339ca913afe91c022886fab25b7c2ff7
DymuX17 <patrykd1707@gmail.com>
2024-10-23 00:48:56 +0200

    first commit with basic version of project

---
+++ init_db_conn.py
@@ -0,0 +1,27 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+
+
+class InitDB:
+    _conn_init = False
+    load_dotenv()
+
+    def __init__(self):
+        if not InitDB._conn_init:
+            self.token = os.getenv("INFLUX_TOKEN")
+            self.org = os.getenv("INFLUX_ORG")
+            self.url = os.getenv("INFLUX_URL")
+            self.bucket = os.getenv("INFLUX_BUCKET")
+            self.client = None
+            self.write_api = None
+            self.query_api = None
+
+            self.connect()
+            InitDB._conn_init = True
+
+    def connect(self):
+        self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org)
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+        self.query_api = self.client.query_api()
---
+++ main.py
@@ -0,0 +1,9 @@
+from get_data import GetData
+
+
+if __name__ == '__main__':
+
+    data_get = GetData()
+    # data_get.write_data() # test function to generate data
+    data_get.write_sin_data()
+    # data_get.get_values() # test function to display values
