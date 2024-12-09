d4209faddb8a6da1184ef2155d2c71925690e059
DymuX17 <patrykd1707@gmail.com>
2024-12-10 00:04:07 +0100

    changes before improvements

--- init_db_conn.py
+++ init_db_conn.py
@@ -1,7 +1,9 @@
 import influxdb_client, os, time
+import opcua
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from dotenv import load_dotenv
+from opcua import Client
 
 
 class InitDB:
@@ -17,6 +19,9 @@ class InitDB:
             self.client = None
             self.write_api = None
             self.query_api = None
+            self.node = None
+            self.url_opcua = "opc.tcp://192.168.1.100:4840"
+            self.Client = Client(self.url_opcua)
 
             self.connect()
             InitDB._conn_init = True
@@ -25,3 +30,19 @@ class InitDB:
         self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org)
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
         self.query_api = self.client.query_api()
+
+
+'''
+        try:
+            self.Client.connect()
+            print("Connected with OPC UA server")
+            self.node = Client.get_node("ns=3;s=a")
+            value = self.node.get_value()
+            print('value: ', value)
+        except Exception as e:
+            print('Error: ', e)
+        finally:
+            self.Client.disconnect()
+            print('Disconnected from OPC UA server...')
+
+'''
